bae46b7bf673849cd33c584bb07f67907efa5964
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-04-09 13:56:54 +0300

    front end and configs

---
+++ Swift_Front_End/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Swift Front End</h1>
+        <p>React + Vite + TypeScript setup</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
